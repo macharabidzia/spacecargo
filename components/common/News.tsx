3a995230c891c211bb0397a33cff05f9e45a862c
machara <macharabidzia@gmail.com>
2025-09-04 15:08:20 +0400

    fix margin news

--- components/common/News.tsx
+++ components/common/News.tsx
@@ -14,12 +14,12 @@ export const NewsSection = ({ lang, newsData }: NewsSectionProps) => {
     lang === "en" ? item.Title_EN : item.Title_GE;
 
   return (
-    <section className="w-full">
+    <section className="w-full mx-10 sm:mx-0">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-20 gap-x-12 justify-items-center">
         {newsData.map((item: NewsItem, index: number) => (
           <Card
             key={item.id}
-            className="w-full relative max-w-sm rounded-lg border bg-card text-card-foreground shadow-lg overflow-visible"
+            className="w-full relative  rounded-lg border bg-card text-card-foreground shadow-lg overflow-visible"
           >
             <CardContent className="pt-4 h-52">
               <div className="absolute -top-10 -left-6 w-full h-full max-h-65 overflow-hidden rounded-lg shadow-xl">
