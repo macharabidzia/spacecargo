df081f4deb32717e73562337bd3f76b6eb8c6576
machara <macharabidzia@gmail.com>
2025-09-04 14:33:15 +0400

    viewport added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export const metadata: Metadata = {
       },
     ],
   },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
   description: siteConfig.description,
 };
 export default async function RootLayout(props: {
--- components/features/dashboard/drawers/EditDeclarationDrawer.tsx
+++ components/features/dashboard/drawers/EditDeclarationDrawer.tsx
@@ -1,4 +1,3 @@
-// components/EditDeclarationDrawer.tsx
 "use client";
 
 import {
